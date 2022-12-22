ac9c120635074e4d065bb7a844777e0c46d1f2ed
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-12-22 23:13:25 +0530

    TouchableOpacity

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import {StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
+let color = 'rgba(255, 125, 125, 0.5)';
 export default function App() {
+  let x = 0;
+  let arr = [...Array(20)].map(() => { return Math.floor(Math.random() * 30) });
+  console.log(arr[0]);
+
+  function handleClick(e){ console.log(e.target.value) }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App to start working on your app!</Text>
+      <Text>{x}</Text>
+
+
+      <View>{arr.map((k, i) => {
+        return (
+        <TouchableOpacity key={i}>
+          <Text onPress={handleClick}>{k}</Text>
+        </TouchableOpacity>
+        )
+      })}
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// http://localhost:19000/debugger-ui/
 // npm i --dev eslint babel-eslint eslint eslint-plugin-react eslint-plugin-react-native
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: color,
+    flex: 1,
     justifyContent: 'center',
   },
 });
