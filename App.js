73458c00b59c4afd55dc34ddab5049e9bbb72096
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2022-12-26 15:03:38 +0530

    react native gestures

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { store } from './store';
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import 'react-native-gesture-handler';
 
 export default function App() {
-
+  const Stack = createStackNavigator();
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <View style={styles.container}>
-          <StatusBar style="auto" />
-          <HomeScreen />
-          <Text style={styles.txt}>UBER</Text>
-        </View>
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
 
   );
@@ -24,14 +36,14 @@ export default function App() {
 // http://localhost:19000/debugger-ui/
 // npm i --dev eslint babel-eslint eslint eslint-plugin-react eslint-plugin-react-native
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    // backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: 'center',
-  },
-  txt: {
-    fontSize: 50,
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     alignItems: 'center',
+//     // backgroundColor: "#fff",
+//     flex: 1,
+//     justifyContent: 'center',
+//   },
+//   txt: {
+//     fontSize: 50,
+//   }
+// });
--- screens/NavOptions.js
+++ screens/NavOptions.js
@@ -2,8 +2,11 @@ import { Text, Image, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Icon } from 'react-native-vector-icons/icon';
 import tw from 'tailwind-react-native-classnames';
+import { useNavigation } from '@react-navigation/native';
 
 const NavOptions = () => {
+
+    const navigation = useNavigation();
     const data = [
         {
             id: "123",
@@ -20,13 +23,18 @@ const NavOptions = () => {
     ];
 
     const renderItem = ({ item }) => {
-        <TouchableOpacity>
+        <TouchableOpacity 
+            onPress={() => navigation.navigate(item.screen)}
+            style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
             <View>
                 <Image
                     style={styles.img}
                     source={{ uri: item.image }}
                 />
-                <Text>{item.title}</Text>
+                <Text style={tw`mt-2 text-lg font-semibold`}>
+                    {item.title}
+                </Text>
                 <Icon
                     style={tw`p-2 bg-black rounded-full w-10 mt-4`}
                     name="arrowright"
@@ -49,7 +57,7 @@ const NavOptions = () => {
 
 const styles = StyleSheet.create({
     img: {
-        height: 120, 
+        height: 120,
         resizeMode: "contain",
         width: 120,
     }
